429e3d47d5ceab3e81007ac40356bfbca1902692
Robert Lesser <rdlesser@gmail.com>
2021-03-10 19:06:46 +0200

    Created the projectile script and attached it to the projectile prefab

---
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+    
+    private Rigidbody2D _rigidbody2D;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _rigidbody2D.velocity = new Vector2(0, _speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.CompareTag("Ceiling"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
